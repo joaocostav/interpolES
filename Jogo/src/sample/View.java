d0276f2c4efa513429ac5119c85147e71e15229b
costafigueira <joaovitor.sc24@gmail.com>
2019-09-09 22:47:31 -0300

    Adicionado javafx no classpath

--- Jogo/src/sample/View.java
+++ Jogo/src/sample/View.java
@@ -1,16 +1,8 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
 
 public class View extends Application {
 
